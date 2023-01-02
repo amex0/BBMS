1611bb1ee7ce5e8dcb57e675b04f9cd66c30693b
AMANUEL <amanueltsehay15@gmail.com>
2023-01-02 14:23:25 +0300

    first commit

---
+++ Blood-Bank-And-Donation-Management-System/footer.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+  <style>
+  #footer {
+
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 65px;
+  background-color:rgba(200,10,10,0.7);
+  color:white;
+  text-align: center;
+}
+  </style>
+</head>
+<body>
+  <div id="footer" >
+  <b><center>COPYRIGHT © 2023<br>
+  Blood Bank Management system RVU MSc students project
+  <br>
+  All Rights Reserved.
+  </center>
+
+  </div>
+
+
+</body>
+
+</html>
